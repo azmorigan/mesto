3a1fda3f06db2ea34e6c7efeb6a8a3d84ee558a3
Rinchin Angarkhaev <rin4in.2014@yandex.ru>
2020-11-24 22:24:45 +0300

    feat: добавить редактирование имени и деятельности

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let popup = document.querySelector('.popup')
-let editPopap = document.querySelector('.profile__edit-button')
-let closePopap = document.querySelector('.popup__close')
+const popup = document.querySelector('.popup')
+const editPopap = document.querySelector('.profile__edit-button')
+const closePopap = document.querySelector('.popup__close')
+const formElement = document.querySelector('.form')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubtitle = document.querySelector('.profile__subtitle')
 
 function openPopup() {
   popup.classList.add('popup__opened')
@@ -10,5 +13,17 @@ function closePopup() {
   popup.classList.remove('popup__opened')
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault()
+
+  let nameInput = formElement.querySelector('.form__name')
+  let careerInput = formElement.querySelector('.form__career')
+
+  profileTitle.textContent = nameInput.value
+  profileSubtitle.textContent = careerInput.value
+  closePopup()
+}
+
 editPopap.addEventListener('click', openPopup)
 closePopap.addEventListener('click', closePopup)
+formElement.addEventListener('submit', formSubmitHandler)
