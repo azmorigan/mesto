73683b1a155156dd4a61ef5e1a5e2babb8ef27b6
Rinchin Angarkhaev <rin4in.2014@yandex.ru>
2020-11-28 13:14:56 +0300

    fix: добавить в profile__info grid

--- script/index.js
+++ script/index.js
@@ -1,26 +1,26 @@
 let popup = document.querySelector('.popup')
 let editPopap = document.querySelector('.profile__edit-button')
 let closePopap = document.querySelector('.popup__close')
-let formElement = document.querySelector('.form')
 let profileTitle = document.querySelector('.profile__title')
 let profileSubtitle = document.querySelector('.profile__subtitle')
+let formElement = document.querySelector('.form')
+let nameInput = formElement.querySelector('.form__input_name')
+let jobInput = formElement.querySelector('.form__input_job')
 
 function openPopup() {
-  popup.classList.add('popup__opened')
+  nameInput.value = profileTitle.textContent
+  jobInput.value = profileSubtitle.textContent
+  popup.classList.add('popup_opened')
 }
 
 function closePopup() {
-  popup.classList.remove('popup__opened')
+  popup.classList.remove('popup_opened')
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault()
-
-  let nameInput = formElement.querySelector('.form__name')
-  let careerInput = formElement.querySelector('.form__career')
-
   profileTitle.textContent = nameInput.value
-  profileSubtitle.textContent = careerInput.value
+  profileSubtitle.textContent = jobInput.value
   closePopup()
 }
 
