3240f6858ad1fdaaebe7018e89b4f3da2518e120
Rinchin Angarkhaev <rin4in.2014@yandex.ru>
2020-12-11 12:38:08 +0300

    feat: open full-size picture

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,39 @@
 // Попап редактирования профиля
-let popup = document.querySelector('.popup')
-let editPopup = document.querySelector('.profile__edit-button')
-let closePopup = document.querySelector('.popup__close')
-let profileTitle = document.querySelector('.profile__title')
-let profileSubtitle = document.querySelector('.profile__subtitle')
+const popup = document.querySelector('.popup')
+const editPopup = document.querySelector('.profile__edit-button')
+const closePopup = document.querySelector('.popup__close')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubtitle = document.querySelector('.profile__subtitle')
 // Форма
-let formElement = document.querySelector('.form')
-let popupAddCard = document.querySelector('.popup_type_add-card')
+const formElement = document.querySelector('.form')
+const popupAddCard = document.querySelector('.popup_type_add-card')
 // Попап добавления карточек
-let buttonClosePopupAddCard = document.querySelector('.popup__close_type_add-card')
+const buttonClosePopupAddCard = document.querySelector('.popup__close_type_add-card')
 // Имя и деятельность в input
 let nameInput = formElement.name
 let jobInput = formElement.job
-// template карточки
-const templateCard = document.querySelector('#template-card')
 // Список карточек
-const listCards = document.querySelector('.elements__list')
+let listCards = document.querySelector('.elements__list')
 // Кнопка открытия формы добавления карточки
 const editPopupAddCardButton = document.querySelector('.profile__add-button')
 // Форма добавления карточек
 const formAddCard = document.querySelector('.form_type_add-card')
 // Название карточки и ссылка на картинку через input
-const placeInput = formAddCard.place
-const linkInput = formAddCard.link
+let placeInput = formAddCard.place
+let linkInput = formAddCard.link
 // 6 карточек
 const initialCards = [
   {
-    name: 'Карачаевск',
-    link: './images/karachaevsk.jpg'
+    name: 'Улаганский район',
+    link: './images/ulaganskiy-rayon.jpg'
   },
   {
-    name: 'Гора Эльбрус',
-    link: './images/elbrus.jpg'
+    name: 'Челябинская область',
+    link: './images/chelyabinck.jpg'
   },
   {
-    name: 'Домбай',
-    link: './images/dombai.jpg'
+    name: 'Москва',
+    link: './images/moscow-city.jpg'
   },
   {
     name: 'Байкал',
@@ -57,17 +55,44 @@ function renderList() {
 }
 // Составить элемент используя свойства объектов
 function composeItem(item) {
+  const templateCard = document.querySelector('#template-card')
   const newItem = templateCard.content.cloneNode(true)
   const itemSrc = newItem.querySelector('.element__img')
   itemSrc.setAttribute('src', `${item.link}`)
   itemSrc.setAttribute('alt', `${item.name}`)
-  const itemPlace = newItem.querySelector('.element__title')
-  itemPlace.textContent = item.name
+  const itemPlaceName = newItem.querySelector('.element__title')
+  itemPlaceName.textContent = item.name
   const removeCardButton = newItem.querySelector('.element__remove')
   removeCardButton.addEventListener('click', removeItem)
   const likeButton = newItem.querySelector('.element__like')
   likeButton.addEventListener('click', likeCard)
+  const itemImage = newItem.querySelector('.element__img')
+  itemImage.addEventListener('click', () => {
+    openModal(item.link, item.name)
+  })
+  const closeModalButton = document.querySelector('.modal__close-button')
+  closeModalButton.addEventListener('click', closeModal)
   return newItem
+}
+// Открыть модальное окно
+
+function openModal(img, name) {
+  let modal = document.querySelector('.modal')
+  const imageModal = modal.querySelector('.modal__image')
+  const nameModal = modal.querySelector('.modal__title')
+  imageModal.src = img
+  imageModal.alt = name
+  nameModal.textContent = name
+  // modal.classList.add('popup_opened') -
+  // - почему-то через добавление модификатора не срабатывает
+  // хотя в девтулзах класс добавляется
+  modal.style.display = 'flex'
+
+}
+// Закрыть модальное окно
+function closeModal(event) {
+  event.target.closest('.modal').style.display = "none"
+  // modal.classList.remove('popup_opened')
 }
 // Поставить лайк
 function likeCard(event) {
@@ -120,3 +145,4 @@ editPopupAddCardButton.addEventListener('click', openPopupAddCard)
 buttonClosePopupAddCard.addEventListener('click', closePopupAddCard)
 formElement.addEventListener('submit', formSubmitHandler)
 formAddCard.addEventListener('submit', formSubmitHandlerAddNewItem)
+
